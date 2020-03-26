4c293ac1720bcb6048c41dfb9dd3701dd7937b79
Julian Finkler <julian@mintware.de>
2020-03-27 00:03:35 +0100

    Updated Angular to remove vulnerable dependencies

--- src/app/presentation/todo-list/todo-list.presenter.ts
+++ src/app/presentation/todo-list/todo-list.presenter.ts
@@ -1,7 +1,9 @@
 import {ShowToDoListPresenter} from '../../core/use-case';
 import {TodoListViewModel} from './todo-list.view-model';
 import {ToDo} from '../../core/entity';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class TodoListPresenter extends ShowToDoListPresenter<TodoListViewModel> {
 
   constructor() {
